bd1723c5c40daea14f621d7ed259ca304aa9a70a
Kathleen Moran <kathleenmoran10@gmail.com>
2022-04-26 20:50:59 -0400

    Add new substrings to hash without using fetch

--- script.rb
+++ script.rb
@@ -1,11 +1,11 @@
 def substrings(words, valid_substrings)
-  substrings_in_words = Hash.new
+  substrings_in_words = Hash.new(0)
   words.split.each do |word|
     valid_substrings.each do |substring|
       if word.downcase.include?(substring.downcase)
-        substrings_in_words[substring] = substrings_in_words.fetch(substring.downcase, 0) + 1
+        substrings_in_words[substring] += 1
       end
     end
   end
-  substrings_in_words
+  p substrings_in_words
 end
